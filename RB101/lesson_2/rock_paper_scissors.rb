e3a1d248b16e5a3cc17d61d902ba3a2d0f368df6
Scott Entwistle <scottyent@gmail.com>
2022-06-29 22:39:33 +0200

    Refactored display_results in rock_paper_scissors for clarity

--- RB101/lesson_2/rock_paper_scissors.rb
+++ RB101/lesson_2/rock_paper_scissors.rb
@@ -4,14 +4,16 @@ def prompt(message)
   puts "=> #{message}"
 end
 
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
+
 def display_results(player, computer)
-  if (player == 'rock' && computer == 'scissors') ||
-    (player == 'paper' && computer == 'rock') ||
-      (player == 'scissors' && computer == 'paper')
+  if win?(player, computer)
     prompt("You won!\u{1F389}")
-  elsif (player == 'rock' && computer == 'paper') ||
-        (player == 'paper' && computer == 'scissors') ||
-          (player == 'scissors' && computer == 'rock')
+  elsif win?(computer, player)
     prompt("Computer won!")
   else
     prompt("Tie")
