4e12d0f19c2abe7407e3803ad94668c2fc7347d8
Scott Entwistle <scottyent@gmail.com>
2022-07-31 22:54:56 +0200

    Add 1 Small Problems Easy 4

--- RB101/small_problems/easy_4/string_to_integer.rb
+++ RB101/small_problems/easy_4/string_to_integer.rb
@@ -17,9 +17,20 @@
   # so the math is, go through and get ascii numbers, then subtract 48 to get the ASCII coordinates of the number
 # Perform some calculation to figure out the number
 # Somehow combine them?! but not string combine them, number combine them in order to return the full number. yikes
+# I'm just going to start coding this as I still don't know how to actually make them one big number
+# Ok - using places to multiple the numbers by 10, 100, 1000's etc and then add it to the places as you go
+# How to do this dynamically....
+
+def string_to_integer(string_number)
+  number_array = string_number.bytes.map { |ascii_value| ascii_value - 48 }.reverse
+  numerical_value = number_array.shift
+
+  number_array.each_with_index { |number, index| numerical_value += number * (10**(index + 1)) }
+  numerical_value
+end
 
 
 
 # Examples/Tests
-string_to_integer('4321') == 4321
-string_to_integer('570') == 570
+p string_to_integer('4321') == 4321
+p string_to_integer('570') == 570
