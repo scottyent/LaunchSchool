7b4cc47e9eba98706bdf15a63d6bda003ea019bd
Scott Entwistle <scottyent@gmail.com>
2022-08-03 23:20:34 +0200

    Add easy_5 Small Programs directory and first problem solved

---
+++ RB101/small_problems/easy_5/ascii_string.rb
@@ -0,0 +1,29 @@
+require 'pry'
+require 'pry-byebug'
+
+# Write a method that determines and returns the ASCII string value of a string that is passed in as an argument.
+# The ASCII string value is the sum of the ASCII values of every character in the string.
+# (You may use String#ord to determine the ASCII value of a character.)
+
+# Input: A string
+# Output: an integer representing the sum of the ASCII values of each character
+
+# Algo
+# Seperate into an array
+# Iterate over it all, using perhaps inject to sum it all up
+# will the empty space mess me up?
+
+def ascii_value(string)
+  ascii_sum = 0
+  string.each_char { |char| ascii_sum += char.ord }
+  binding.pry
+  ascii_sum
+end
+
+
+
+# Tests
+p ascii_value('Four score') == 984
+p ascii_value('Launch School') == 1251
+p ascii_value('a') == 97
+p ascii_value('') == 0
