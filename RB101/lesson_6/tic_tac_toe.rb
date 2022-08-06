6f38c1038111a2f3fdff1e38ea4748422fe4efea
Scott Entwistle <scottyent@gmail.com>
2022-08-06 14:02:40 +0200

    Add Lesson 6 TicTacToe and 1 Easy 5 problem

---
+++ RB101/lesson_6/tic_tac_toe.rb
@@ -0,0 +1,31 @@
+# Description of the game in my own words:
+# Tic Tac Toe is a game that we will create with one player and a computer player. The point of the game is to get
+# Three of your symbols in a row within a 3 x 3 grid
+# Players alternate turns until one is able to have 3 in a row, or there are no more playable moves
+
+# Super similar to their description:
+# Tic Tac Toe is a 2 player game played on a 3x3 board. Each player takes a turn and
+# marks a square on the board. First player to reach 3 squares in a row, including diagonals,
+# wins. If all 9 squares are marked and no player has 3 squares in a row, then the game is a tie.
+
+# Breaking it down to build a flow chart
+# Display the board
+# Allow first user to make a choice
+# Place that users symbol in that space
+# Did that player win? Is there a tie?
+# If not, next player gets to make a choice and place their symbol
+# Did that player win? Is there a tie?
+# Next player. etc
+
+# Their version, a bit more elegantly outlined:
+# 1. Display the initial empty 3x3 board.
+# 2. Ask the user to mark a square.
+# 3. Computer marks a square.
+# 4. Display the updated board state.
+# 5. If winner, display winner.
+# 6. If board is full, display tie.
+# 7. If neither winner nor board is full, go to #2
+# 8. Play again?
+# 9. If yes, go to #1
+# 10. Good bye!
+
---
+++ RB101/small_problems/easy_5/after_midnight_2.rb
@@ -0,0 +1,39 @@
+# Input: "00:00" formatted string, representing 24 hour time
+# Output: the minutes before or after midnight (two separate methods)
+
+# Rules
+# Two separate methods, one for before midnight and one for after
+# Can't use Date and Time methods
+# can take in 24:00 to represent midnight as well
+
+# Algo
+# Split the input into an array with integers in them, representing hours and minutes
+# Multiply the hours to get minutes, and merge it into the minutes function
+# Use the minutes function to calculate how far from midnight in respective direction
+
+require 'pry'
+require 'pry-byebug'
+
+MINUTES_PER_HOUR = 60
+
+
+def after_midnight(time_string)
+  hours, minutes = time_string.split(":").map { |num_string| num_string.to_i }
+  hours = 0 if hours == 24
+  minutes += hours * MINUTES_PER_HOUR
+end
+
+def before_midnight(time_string)
+  hours, minutes = time_string.split(":").map { |num_string| num_string.to_i }
+  hours = 0 if hours == 24
+  minutes += hours * MINUTES_PER_HOUR
+  minutes == 0 ? 0 : (1440 - minutes)
+end
+
+# Tests
+p after_midnight('00:00') == 0
+p before_midnight('00:00') == 0
+p after_midnight('12:34') == 754
+p before_midnight('12:34') == 686
+p after_midnight('24:00') == 0
+p before_midnight('24:00') == 0
