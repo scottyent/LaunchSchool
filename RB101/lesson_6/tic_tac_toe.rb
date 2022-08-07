bcef2ec627951983e0fa319996fbeb1b5fbf44b0
Scott Entwistle <scottyent@gmail.com>
2022-08-07 17:55:57 +0200

    Update tictactoe up to detecting winner, with my logic

--- RB101/lesson_6/tic_tac_toe.rb
+++ RB101/lesson_6/tic_tac_toe.rb
@@ -18,7 +18,7 @@
 # Next player. etc
 
 # Their version, a bit more elegantly outlined:
-# 1. Display the initial empty 3x3 board.
+# 1. Display the initial empty 3x3 board. :DONE
 # 2. Ask the user to mark a square.
 # 3. Computer marks a square.
 # 4. Display the updated board state.
@@ -31,7 +31,17 @@
 require 'pry'
 require 'pry-byebug'
 
+DEFAULT_SPACE_VALUE = " "
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
 def display_board(board_state)
+  system 'clear'
   puts ""
   puts "     |     |"
   puts "  #{board_state[1]}  |  #{board_state[2]}  |  #{board_state[3]}"
@@ -49,9 +59,70 @@ end
 
 def initialize_board()
   new_board = {}
-  (1..9).each { |num| new_board[num] = ' ' }
+  (1..9).each { |num| new_board[num] = DEFAULT_SPACE_VALUE }
   new_board
+end
+
+def empty_squares(board)
+  board.keys.select{ |num| board[num] == DEFAULT_SPACE_VALUE }
+end
+
+def player_turn!(board)
+  square = ''
+  loop do
+    prompt "Choose a square (#{empty_squares(board).join(", ")}):"
+    square = gets.chomp.to_i
+    break if empty_squares(board).include?(square)
+    prompt "Either that spot is taken, or your entry is invalid."
+  end
+
+  board[square] = PLAYER_MARKER
+end
+
+def computer_turn!(board)
+  computer_square = empty_squares(board).sample
+  board[computer_square] = COMPUTER_MARKER
+end
+
+def board_full?(board)
+  empty_squares(board).empty?
+end
+
+def detect_winner(board)
+  # I've written this with my logic, but will update to his logic once I watch the video
+  winning_conditions = [[1, 2, 3], [4, 5, 6], [7, 8, 9] ,[1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+  winning_conditions.each do |row_array|
+    row_status = []
+    row_array.each do |square_number|
+      row_status << board[square_number]
+    end
+
+    if row_status.eql?(["X", "X", "X"])
+     return "Player"
+    elsif row_status.eql?(["O", "O", "O"])
+     return "Computer"
+    end
+
+  end
+
+  false
+end
+
+def someone_won?(board)
+  detect_winner(board)
 end
 
 board = initialize_board
 display_board(board)
+loop do
+  player_turn!(board)
+  computer_turn!(board)
+  display_board(board)
+  break if someone_won?(board) || board_full?(board)
+end
+
+if someone_won?(board)
+  prompt "#{detect_winner(board)} won!"
+else
+  prompt "It's a tie."
+end
