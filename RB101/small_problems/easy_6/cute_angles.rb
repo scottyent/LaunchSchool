cd44df7a1c47dbd34e4ef3cc25ace875a303f4b6
Scott Entwistle <scottyent@gmail.com>
2022-09-05 21:54:38 +0200

    Add further exploration for first Easy 6 problem

--- RB101/small_problems/easy_6/cute_angles.rb
+++ RB101/small_problems/easy_6/cute_angles.rb
@@ -7,12 +7,25 @@
 # Isolate the part of the number left of the decimal, use that for minutes
 # Multiply the part right of the decimal use that for seconds
 # Format all into a nice string
+
+# Further Exploration
+# Handle numbers larger than 360 or negative
+# If they're larger than 360, subtract 360.
+# If they're negative, subtract them from 360.
+# If they're bigger than 360 and negative, subtract 360 first than subtract from 360
+
 require 'pry'
 require 'pry-byebug'
 
 DEGREE = "\xC2\xB0"
 
 def dms(degree)
+  if degree.abs > 360
+    degree = degree.positive? ? degree % 360 : (360 - (degree.abs % 360))
+  elsif degree < 0
+    degree = 360 + degree
+  end
+
   converted = []
 
   while converted.size < 3
@@ -33,3 +46,8 @@ p dms(254.6) == %(254°36'00")
 p dms(93.034773) == %(93°02'05")
 p dms(0) == %(0°00'00")
 p dms(360) == %(360°00'00") || dms(360) == %(0°00'00")
+
+# Further Exploration
+p dms(400) == %(40°00'00")
+p dms(-40) == %(320°00'00")
+p dms(-420) == %(300°00'00")
