fdf4f0a053f09d0cded39a9f4cfd2692592e16ba
Scott Entwistle <scottyent@gmail.com>
2022-09-19 22:09:21 +0200

    Add Easy 7 Small Problems staggered_caps_2.rb

---
+++ RB101/small_problems/easy_7/staggered_caps_2.rb
@@ -0,0 +1,33 @@
+# Input: String
+# Output: Same as before, but non alphabetic characters don't count
+
+require 'pry'
+require 'pry-byebug'
+
+def staggered_case(words, start_caps=true, count_non_alphabetic=true)
+  even_odd_counter = start_caps ? 0 : 1
+
+  fixed_array = words.chars.map do |char|
+    transformed_letter = even_odd_counter.even? ? char.upcase : char.downcase
+    #binding.pry
+    if count_non_alphabetic
+      even_odd_counter += 1
+    else
+      even_odd_counter += 1 if char =~ /[A-Za-z]/
+    end
+
+    transformed_letter
+  end
+
+  fixed_array.join
+end
+
+# Test cases
+p staggered_case('I Love Launch School!', start_caps=true, count_non_alphabetic = false) == 'I lOvE lAuNcH sChOoL!'
+p staggered_case('ALL CAPS', start_caps=true, count_non_alphabetic=false) == 'AlL cApS'
+p staggered_case('ignore 77 the 444 numbers', start_caps=true, count_non_alphabetic=false) == 'IgNoRe 77 ThE 444 nUmBeRs'
+
+p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
+p staggered_case('ALL_CAPS') == 'AlL_CaPs'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
+
