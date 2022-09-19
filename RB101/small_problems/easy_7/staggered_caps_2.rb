4f7f9e7f096abb8861569bfbcb7d945f47137de0
Scott Entwistle <scottyent@gmail.com>
2022-09-19 22:22:41 +0200

    Fix keyword arguments for staggered_caps_2.rb

--- RB101/small_problems/easy_7/staggered_caps_2.rb
+++ RB101/small_problems/easy_7/staggered_caps_2.rb
@@ -4,12 +4,12 @@
 require 'pry'
 require 'pry-byebug'
 
-def staggered_case(words, start_caps=true, count_non_alphabetic=true)
+def staggered_case(words, start_caps: true, count_non_alphabetic: true)
   even_odd_counter = start_caps ? 0 : 1
 
   fixed_array = words.chars.map do |char|
     transformed_letter = even_odd_counter.even? ? char.upcase : char.downcase
-    #binding.pry
+
     if count_non_alphabetic
       even_odd_counter += 1
     else
@@ -23,9 +23,9 @@ def staggered_case(words, start_caps=true, count_non_alphabetic=true)
 end
 
 # Test cases
-p staggered_case('I Love Launch School!', start_caps=true, count_non_alphabetic = false) == 'I lOvE lAuNcH sChOoL!'
-p staggered_case('ALL CAPS', start_caps=true, count_non_alphabetic=false) == 'AlL cApS'
-p staggered_case('ignore 77 the 444 numbers', start_caps=true, count_non_alphabetic=false) == 'IgNoRe 77 ThE 444 nUmBeRs'
+p staggered_case('I Love Launch School!', count_non_alphabetic: false) == 'I lOvE lAuNcH sChOoL!'
+p staggered_case('ALL CAPS', count_non_alphabetic: false) == 'AlL cApS'
+p staggered_case('ignore 77 the 444 numbers', count_non_alphabetic: false) == 'IgNoRe 77 ThE 444 nUmBeRs'
 
 p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
 p staggered_case('ALL_CAPS') == 'AlL_CaPs'
