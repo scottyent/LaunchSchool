b5f08fa2c004dcaec23b5892fbf3ff0d26a41ae0
Scott Entwistle <scottyent@gmail.com>
2022-09-29 15:02:32 +0200

    Refactor Medium 1 stack_machine.rb to handle empty stacks, and to include the further exploration

--- RB101/small_problems/medium_1/stack_machine.rb
+++ RB101/small_problems/medium_1/stack_machine.rb
@@ -32,6 +32,15 @@
 require 'pry'
 require 'pry-byebug'
 
+def display_error_message(type)
+  messages = {
+    stack: "There wasn't a value in the stack to pull",
+    command: "That is not a valid command"
+  }
+
+  puts messages[type]
+end
+
 
 def display_register(current_count)
   puts "Register: #{current_count[:register]}"
@@ -44,32 +53,56 @@ end
 
 # * ADD Pops a value from the stack and adds it to the register value, storing the result in the register.
 def add(current_state)
-  current_state[:register] += current_state[:stack].pop
+  begin
+    current_state[:register] += current_state[:stack].pop
+  rescue TypeError
+    display_error_message(:stack) if current_state[:stack].empty?
+  end
 end
 
 # * SUB Pops a value from the stack and subtracts it from the register value, storing the result in the register.
 def subtract(current_state)
-  current_state[:register] -= current_state[:stack].pop
+  begin
+    current_state[:register] -= current_state[:stack].pop
+  rescue TypeError
+    display_error_message(:stack) if current_state[:stack].empty?
+  end
 end
 
 # * MULT Pops a value from the stack and multiplies it by the register value, storing the result in the register.
 def multiply(current_state)
-  current_state[:register] *= current_state[:stack].pop
+  begin
+    current_state[:register] *= current_state[:stack].pop
+  rescue TypeError
+    display_error_message(:stack) if current_state[:stack].empty?
+  end
 end
 
 # * DIV Pops a value from the stack and divides it into the register value, storing the integer result in the register.
 def divide(current_state)
-  current_state[:register] /= current_state[:stack].pop
+  begin
+    current_state[:register] /= current_state[:stack].pop
+  rescue TypeError
+    display_error_message(:stack) if current_state[:stack].empty?
+  end
 end
 
 # * MOD Pops a value from the stack and divides it into the register value, storing the integer remainder of the division in the register.
 def remainder(current_state)
-  current_state[:register] %= current_state[:stack].pop
+  begin
+    current_state[:register] %= current_state[:stack].pop
+  rescue TypeError
+    display_error_message(:stack) if current_state[:stack].empty?
+  end
 end
 
 # * POP Remove the topmost item from the stack and place in register
 def remove(current_state)
-  current_state[:register] = current_state[:stack].pop
+  begin
+    current_state[:register] = current_state[:stack].pop
+  rescue TypeError
+    display_error_message(:stack) if current_state[:stack].empty?
+  end
 end
 
 def minilang(commands)
@@ -99,40 +132,55 @@ def minilang(commands)
     when "POP" then remove(state)
     when "PRINT" then display_register(state)
     else
-      puts "That is not a valid command"
+      display_error_message(:command)
     end
   end
+
+  nil
 end
 
 # Test Cases
-minilang('PRINT')
+# minilang('PRINT')
 # 0
 
-minilang('5 PUSH 3 MULT PRINT')
+# minilang('5 PUSH 3 MULT PRINT')
 # 15
 
-minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
+# minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
 # 5
 # 3
 # 8
 
-minilang('5 PUSH POP PRINT')
+# minilang('5 PUSH POP PRINT')
 # 5
 
-minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')
+# minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')
 # 5
 # 10
 # 4
 # 7
 
-minilang('3 PUSH PUSH 7 DIV MULT PRINT ')
+# minilang('3 PUSH PUSH 7 DIV MULT PRINT ')
 # 6
 
-minilang('4 PUSH PUSH 7 MOD MULT PRINT ')
+# minilang('4 PUSH PUSH 7 MOD MULT PRINT ')
 # 12
 
-minilang('-3 PUSH 5 SUB PRINT')
+# minilang('-3 PUSH 5 SUB PRINT')
 # 8
 
-minilang('6 PUSH')
+# minilang('6 PUSH')
 # (nothing printed; no PRINT commands)
+
+# Further Exploration
+# Translate this -> (3 + (4 * 5) - 7) / (5 % 3)
+# Into a command to run for the minilang program
+# Result is equal to 8
+# Results:
+
+minilang("3 PUSH 5 MOD PUSH 7 PUSH 4 PUSH 5 MULT PUSH 3 ADD SUB DIV PRINT")
+
+# More further exploration - handle empty stack situations and wrong commands
+# Wrong commans already done by having that at the end of the else
+# empty stack...
+# I did this but it got very out of hand and is very clunky with these mini methods
