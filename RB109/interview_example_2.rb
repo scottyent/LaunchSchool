c402aa00c390fdda8d26e1dfce4c462f3d14230a
Scott Entwistle <scottyent@gmail.com>
2022-10-12 23:01:39 +0200

    Update interview_example_2.rb with a different strategy from the video

--- RB109/interview_example_2.rb
+++ RB109/interview_example_2.rb
@@ -20,12 +20,35 @@
 # iterate through the array, and if larger number is found, return -1
 # If a larger number IS found, return on the first one since it's sorted
 
-def next_bigger_num(num)
-  number_array = num.to_s.chars
-  num_perms = []
-  number_array.permutation { |perm| num_perms << perm.join.to_i }
+# My initial solution
+# def next_bigger_num(num)
+#   number_array = num.to_s.chars
+#   num_perms = []
+#   number_array.permutation { |perm| num_perms << perm.join.to_i }
 
-  num_perms.sort.each { |sort_num| return sort_num if sort_num > num }
+#   num_perms.sort.each { |sort_num| return sort_num if sort_num > num }
+
+#   -1
+# end
+
+# Written algorithm following the logic in the example he gave
+# Iterate from 0 to the max number it can be (so num.to_s.chars.sort {|a, b| b <=> a }).join.to_i
+# check if the item has the same sigits AND is larger than the current number
+# if you hit, return it, otherwise return -1
+
+# Ok attempting to writ it out using the logic they've implied in the video so far
+# It worked! But at first I did 0 up to biggest num instead of just num to biggest num
+
+def same_digits?(num1, num2)
+  num1.digits.sort == num2.digits.sort
+end
+
+def next_bigger_num(num)
+  biggest_num = num.digits.sort { |a, b| b <=> a }.join.to_i
+
+  num.upto(biggest_num) do |check_num|
+    return check_num if same_digits?(num, check_num) && check_num > num
+  end
 
   -1
 end
@@ -34,6 +57,7 @@ p next_bigger_num(9) == -1
 p next_bigger_num(12) == 21
 p next_bigger_num(513) == 531
 p next_bigger_num(2017) == 2071
+p next_bigger_num(2071) == 2107
 p next_bigger_num(111) == -1
 p next_bigger_num(531) == -1
 p next_bigger_num(123456789) == 123456798
