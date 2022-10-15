d88dd54ada6b58a149c1cf6234356c0c6ecc0c30
Scott Entwistle <scottyent@gmail.com>
2022-10-15 13:33:41 +0200

    Add Medium 2 matching_parentheses.rb solved

---
+++ RB101/small_problems/medium_2/matching_parentheses.rb
@@ -0,0 +1,53 @@
+=begin
+Input: String
+Output: boolean true/false
+
+Rules
+The goal is to check whether parentheses are properly balanced
+Meaning that they are matching pairs
+So the challenge here is that it isn't a simple count
+It's tracking whether they match appropriately
+E.g. 'What ((is))) up(' has three opening, and three closing
+but they're not valid.
+
+Steps:
+Iterate through the charactes
+keep track of open parenth and close parantheses
+close should never get above the opens
+and only test the ones where they start of matching
+
+return false if the amount of open and close parenth aren't equal
+set variable string_array to .chars
+initialize open_parenth and close_parenth to zero
+
+iterate through the array
+if the character is an open or close parenth, increment that variable
+return false if closed is greater than open
+
+otherwise return true
+=end
+
+# def balanced?(string)
+#   return false if string.count('(') != string.count(')')
+#   string_array = string.chars
+#   open_parenth = 0
+#   close_parenth = 0
+
+#   string_array.each do |char|
+#     open_parenth += 1 if char == "("
+#     close_parenth += 1 if char == ")"
+#     return false if close_parenth > open_parenth
+#   end
+
+#   true
+# end
+
+# Test Cases
+p balanced?('What (is) this?') == true
+p balanced?('What is) this?') == false
+p balanced?('What (is this?') == false
+p balanced?('((What) (is this))?') == true
+p balanced?('((What)) (is this))?') == false
+p balanced?('Hey!') == true
+p balanced?(')Hey!(') == false
+p balanced?('What ((is))) up(') == false
