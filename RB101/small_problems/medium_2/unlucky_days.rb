b07cdc7e95f893a11890a6c2e7eb80a87bc58036
Scott Entwistle <scottyent@gmail.com>
2022-10-18 22:15:01 +0200

    Add further exploration to unlucky_days.rb

--- RB101/small_problems/medium_2/unlucky_days.rb
+++ RB101/small_problems/medium_2/unlucky_days.rb
@@ -34,3 +34,36 @@ end
 p friday_13th(2015) == 3
 p friday_13th(1986) == 1
 p friday_13th(2019) == 2
+
+
+# Further Exploration
+# Count the number of months that have 5 Fridays
+
+# Steps
+# It says to account for leap years is the challenge
+# I guess I don't undersatnd, perhaps there are 52 fridays on every year that's not a leap
+# But i'll just brute force it, and start a year instance, iterate through every day
+# if it's friday, put the month (numberical) in a dictionary, and increment it
+
+def five_fridays(year)
+  current_date = Date.new(year, 1, 1)
+  months_fridays = {}
+
+  365.times do |_|
+    if current_date.friday?
+      month = current_date.month
+      if months_fridays.key?(month)
+        months_fridays[month] += 1
+      else
+        months_fridays[month] = 1
+      end
+    end
+    current_date = current_date.next_day
+  end
+
+  months_fridays.select { |month, fridays| fridays  == 5 }.count
+end
+
+p five_fridays(2015)
+p five_fridays(1986)
+p five_fridays(2019)
