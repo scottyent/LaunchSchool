3a04129686647be82a2bd08259b1d98bc78244b5
Scott Entwistle <scottyent@gmail.com>
2022-10-27 20:41:18 +0200

    Add codewars sum_of_pairs.rb efficiency attempt

--- RB109/codewars_problems/sum_of_pairs.rb
+++ RB109/codewars_problems/sum_of_pairs.rb
@@ -1,5 +1,3 @@
-# This code works, but fails the efficency test in codewars (times out) need to refactor
-
 =begin
 Input: an array of integers, and a number to sum to
 Output: two numbers that sum to that sum, from within the array
@@ -22,24 +20,47 @@ if number is less than the sum number
 if the number of saved couples is greater than 1, pass to
   a helper function that min_by to_s to the -1 place in the string
 =end
+require 'pry'
+require 'pry-byebug'
+
+# def sum_pairs(arr, sum)
+#   results = {}
+#   second_index = arr.size
+
+#   arr.each_with_index do |number, index|
+#       arr[index + 1..-1].each_with_index do |num,in_idx|
+#         if num + number == sum
+#           second_index = index + in_idx + 1
+#           results[[number, num]] = [second_index]
+#           break
+#         end
+#       end
+
+#     break if results.size == 2
+#   end
+
+#   results.empty? ? nil : results.min_by{ |k, v| v }[0]
+# end
+
+# Old solution works but is too inefficient, I want to reduce to one loop
+
+require 'set'
+require 'benchmark'
 
 def sum_pairs(arr, sum)
   results = {}
 
-  arr.each_with_index do |number, index|
-      arr[index + 1..-1].each_with_index do |num,in_idx|
-        if num + number == sum
-          results[[number, num]] = [index, index + in_idx + 1]
-        end
-      end
+  array_set = arr.to_set
+
+  array_set.each do |number|
+    counter_number = sum - number
+    if array_set.include?(counter_number)
+      results[[counter_number, number]] = arr.index(number)
+    end
+    break if results.size == 2
   end
 
-  if results.keys.size > 1
-    lower_index = results.values.min_by { |pair| pair[-1] }
-    results = results.select { |k, v| v == lower_index }
-  end
-
-  results.empty? ? nil : results.keys.flatten
+  results.empty? ? nil : results.min_by{ |k, v| v }[0]
 end
 
 
@@ -53,11 +74,12 @@ l6= [4, -2, 3, 3, 4]
 l7= [0, 2, 0]
 l8= [5, 9, 13, -3]
 
-p sum_pairs(l1, 8) == [1, 7]
-p sum_pairs(l2, -6) == [0, -6]
-p sum_pairs(l3, -7) == nil
-p sum_pairs(l4, 2) == [1, 1]
-p sum_pairs(l5, 10) == [3, 7]
-p sum_pairs(l6, 8) == [4, 4]
-p sum_pairs(l7, 0) == [0, 0]
-p sum_pairs(l8, 10) == [13, -3]
+puts Benchmark.measure { sum_pairs(l1, 8) }
+# p sum_pairs(l1, 8) == [1, 7]
+# p sum_pairs(l2, -6) == [0, -6]
+# p sum_pairs(l3, -7) == nil
+# p sum_pairs(l4, 2) == [1, 1]
+# p sum_pairs(l5, 10) == [3, 7]
+# p sum_pairs(l6, 8) == [4, 4]
+# p sum_pairs(l7, 0) == [0, 0]
+# p sum_pairs(l8, 10) == [13, -3]
