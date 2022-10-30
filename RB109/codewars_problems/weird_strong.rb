7937d739294b53dc4449b9581c4c763212765968
Scott Entwistle <scottyent@gmail.com>
2022-10-30 14:31:44 +0100

    Add codewars weird_string.rb solved

--- RB109/codewars_problems/weird_strong.rb
+++ RB109/codewars_problems/weird_strong.rb
@@ -4,8 +4,14 @@ Output: a string with even characters upcased, and odd index downcased
 
 
 Rules
-it will only be alpha chars and spaces.
-spaces count in the indexing
+explicit
+- it will only be alpha chars and spaces.
+
+implicit
+ - spaces are not part of the capitalization changes
+ - So it starts new for each word basically
+ - It's case insensitive in that it only matters that the right index is the right size, so we can downcase from the beginning
+  - and then just focus on upcasing what we need to
 
 Steps:
 initialize results string to empty string
@@ -15,36 +21,42 @@ if even index, upcase and shovel it into the string
   otehrwise downcase and shove it
   to simplify this I'll downcase to start
 
-  start over for each word
-  so we'll do
-  split first
+if it's more than one word:
+  iterate through each word with index
+  set that index of the array equal to case_change(word)
+  this will modify the word_array
+  then join with a space between and return that
+end
 =end
+
+def case_change(word)
+  results = ''
+
+  word.chars.each_with_index do |char, index|
+    index.even? ? results << char.upcase : results << char
+  end
+
+  results
+end
+
 
 def weirdcase(str)
   str.downcase!
   word_array = str.split
 
   if word_array.size == 1
-    results = ''
-
-    str.chars.each_with_index do |char, index|
-      index.even? ? results << char.upcase : results << char
-    end
+    case_change(str)
   else
-    fixed_array = word_array.each_with_object([]) do |word, arr|
-      word.chars.each_with_index do |char, index|
-        index.even? ? arr << char.upcase : arr << char
-      end
-      fixedi
+    word_array.each_with_index do |word, index|
+      word_array[index] = case_change(word)
     end
-    results = fixed_array.join(" ")
-  end
 
-    results
+    word_array.join(' ')
+  end
 end
 
 # Test Cases
 
 p weirdcase('This') == 'ThIs'
 p weirdcase('is') == 'Is'
-p weirdcase('This is a test') #== 'ThIs Is A TeSt'
+p weirdcase('This is a test') == 'ThIs Is A TeSt'
