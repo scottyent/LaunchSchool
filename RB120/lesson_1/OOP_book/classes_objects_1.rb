3e5a6f6cdba84aa8fb01c028e801e62627502571
Scott Entwistle <scottyent@gmail.com>
2022-11-01 14:46:43 +0100

    Add problem 3 solved for classes_object_1.rb

--- RB120/lesson_1/OOP_book/classes_objects_1.rb
+++ RB120/lesson_1/OOP_book/classes_objects_1.rb
@@ -91,6 +91,57 @@ Then add an accessor method that allows you to view, but not modify, the year of
 
 Oops, I sort of already did this as I started with accessor methods above, but I'll tweak it for the read only one.
 =end
+# class MyCar
+#   attr_accessor :color, :model, :speed
+#   attr_reader :year
+
+#   def initialize(y, c, m)
+#     @year = y
+#     @color = c
+#     @model = m
+#     @speed = 0
+#   end
+
+#   def speed_up(number)
+#     self.speed += number
+#     puts "You push the accelerator and speed up #{number} MPH"
+#   end
+
+#   def brake(number)
+#     self.speed -= number
+#     puts "You push the break and slow down #{number} MPH"
+#   end
+
+#   def current_speed
+#     puts "You are currently driving #{speed} MPH"
+#   end
+
+#   def turn_off
+#     self.speed = 0
+#     puts "You are now parked!"
+#   end
+# end
+
+# eve = MyCar.new(2021, "White", "Tesla")
+# puts eve.color
+# puts eve.model
+# puts eve.year
+
+# eve.color = "Red"
+# eve.model = "Toyota"
+# # eve.year = 2000              => Throws an error because there is no setter method (year=) for year since it's read only
+
+# puts eve.color
+# puts eve.model
+# puts eve.year
+
+
+# 3
+=begin
+You want to create a nice interface that allows you to accurately describe the action you want your program to perform.
+Create a method called spray_paint that can be called on an object and will modify the color of the car.
+=end
+
 class MyCar
   attr_accessor :color, :model, :speed
   attr_reader :year
@@ -120,17 +171,16 @@ class MyCar
     self.speed = 0
     puts "You are now parked!"
   end
+
+  def spray_paint(color)
+    self.color = color
+    puts "Sweet upgrade! Your car is now #{color}"
+  end
 end
 
 eve = MyCar.new(2021, "White", "Tesla")
 puts eve.color
 puts eve.model
 puts eve.year
-
-eve.color = "Red"
-eve.model = "Toyota"
-# eve.year = 2000              => Throws an error because there is no setter method (year=) for year since it's read only
-
+eve.spray_paint("Red")
 puts eve.color
-puts eve.model
-puts eve.year
