c41f1f57510285f35100338d32795d8d0c505c2e
Scott Entwistle <scottyent@gmail.com>
2022-11-01 22:25:56 +0100

    Add classes_objects_2.rb - chapter exercises done

--- RB120/lesson_1/OOP_book/classes_objects_2.rb
+++ RB120/lesson_1/OOP_book/classes_objects_2.rb
@@ -64,3 +64,102 @@
 #     self
 #   end
 # end
+
+# Exercises
+#1: Add a class method to your MyCar class that calculates the gas mileage of any car.
+# I really overthought this one and added instance variabels to track a specific cars distance and tank size
+# the answer is way more basic:
+
+# class MyCar
+#   def self.gas_mileage(gallons, miles)
+#     puts "#{miles / gallons} miles per gallon of gas"
+#   end
+# end
+
+# MyCar.gas_mileage(13, 351)  # => "27 miles per gallon of gas"
+
+# class MyCar
+#   attr_accessor :color, :model, :speed, :distance, :tank_size
+#   attr_reader :year
+
+#   def initialize(y, c, m, d, t)
+#     @year = y
+#     self.color = c
+#     self.model = m
+#     self.distance = 240
+#     self.tank_size = 12
+#     self.speed = 0
+#   end
+
+#   def self.calculate_mileage(car_object)
+#     puts "The mileage of this car is #{car_object.distance/car_object.tank_size}"
+#   end
+
+#   def self.gas_mileage(gallons, miles)
+#     puts "#{miles / gallons} miles per gallon of gas"
+#   end
+
+#   def speed_up(number)
+#     self.speed += number
+#     puts "You push the accelerator and speed up #{number} MPH"
+#   end
+
+#   def brake(number)
+#     self.speed -= number
+#     puts "You push the break and slow down #{number} MPH"
+#   end
+
+#   def current_speed
+#     puts "You are currently driving #{speed} MPH"
+#   end
+
+#   def turn_off
+#     self.speed = 0
+#     puts "You are now parked!"
+#   end
+
+#   def spray_paint(color)
+#     self.color = color
+#     puts "Sweet upgrade! Your car is now #{color}"
+#   end
+
+#   def to_s
+#     "This is a cool #{color} #{model} from #{year}"
+#   end
+# end
+
+# eve = MyCar.new(2021, "white", "Tesla", 240, 12)
+
+# MyCar.calculate_mileage(eve)
+# MyCar.gas_mileage(12, 240)
+
+# # 2 Override the to_s method to create a user friendly print out of your object.
+
+# puts eve
+
+# 3 when running the following code:
+# class Person
+#   attr_reader :name
+#   def initialize(name)
+#     @name = name
+#   end
+# end
+
+# bob = Person.new("Steve")
+# bob.name = "Bob"
+
+# We get the following error...
+# test.rb:9:in `<main>': undefined method `name=' for
+#   #<Person:0x007fef41838a28 @name="Steve"> (NoMethodError)
+
+# Why do we get this error and how do we fix it?
+# We get this error because we've setup @name to be read only, so we can set it to do both or just set, and run it again:
+class Person
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+bob = Person.new("Steve")
+bob.name = "Bob"
