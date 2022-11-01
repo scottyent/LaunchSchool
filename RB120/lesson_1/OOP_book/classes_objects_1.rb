65b57f51365c9a729585a2b04793fc03ece6e9aa
Scott Entwistle <scottyent@gmail.com>
2022-11-01 14:44:21 +0100

    Add problem 2 solved for classes_objects_1.rb

--- RB120/lesson_1/OOP_book/classes_objects_1.rb
+++ RB120/lesson_1/OOP_book/classes_objects_1.rb
@@ -45,8 +45,55 @@ Create an instance variable that is set to 0 during instantiation of the object 
 speed of the car as well. Create instance methods that allow the car to speed up, brake, and shut the car off.
 =end
 
+# class MyCar
+#   attr_accessor :year, :color, :model, :speed
+
+#   def initialize(y, c, m)
+#     @year = y
+#     @color = c
+#     @model = m
+#     @speed = 0
+#   end
+
+#   def speed_up(number)
+#     self.speed += number
+#     puts "You push the accelerator and speed up #{number} MPH"
+#   end
+
+#   def brake(number)
+#     self.speed -= number
+#     puts "You push the break and slow down #{number} MPH"
+#   end
+
+#   def current_speed
+#     puts "You are currently driving #{speed} MPH"
+#   end
+
+#   def turn_off
+#     self.speed = 0
+#     puts "You are now parked!"
+#   end
+# end
+
+# eve = MyCar.new(2021, "White", "Tesla")
+# eve.speed_up(15)
+# eve.current_speed
+# eve.brake(5)
+# eve.current_speed
+# eve.turn_off
+# eve.current_speed
+
+
+# 2
+=begin
+Add an accessor method to your MyCar class to change and view the color of your car.
+Then add an accessor method that allows you to view, but not modify, the year of your car.
+
+Oops, I sort of already did this as I started with accessor methods above, but I'll tweak it for the read only one.
+=end
 class MyCar
-  attr_accessor :year, :color, :model, :speed
+  attr_accessor :color, :model, :speed
+  attr_reader :year
 
   def initialize(y, c, m)
     @year = y
@@ -76,9 +123,14 @@ class MyCar
 end
 
 eve = MyCar.new(2021, "White", "Tesla")
-eve.speed_up(15)
-eve.current_speed
-eve.brake(5)
-eve.current_speed
-eve.turn_off
-eve.current_speed
+puts eve.color
+puts eve.model
+puts eve.year
+
+eve.color = "Red"
+eve.model = "Toyota"
+# eve.year = 2000              => Throws an error because there is no setter method (year=) for year since it's read only
+
+puts eve.color
+puts eve.model
+puts eve.year
