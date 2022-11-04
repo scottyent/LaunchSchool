27af119bfd4d71fe0f065c424d869fe177fe0b0e
Scott Entwistle <scottyent@gmail.com>
2022-11-04 18:11:23 +0100

    Add small problems hello_sophie_2.rb solved

---
+++ RB120/small_problems/OO_1.rb/hello_sophie_2.rb
@@ -0,0 +1,15 @@
+#  move the greeting from the #initialize method to an
+# instance method named #greet that prints a greeting when invoked.
+
+class Cat
+  def initialize(name)
+    @name = name
+  end
+
+  def greet
+    puts "Hello! My name is #{@name}!"
+  end
+end
+
+kitty = Cat.new('Sophie')
+kitty.greet
