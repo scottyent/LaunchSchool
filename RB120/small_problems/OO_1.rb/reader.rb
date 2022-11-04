85f67caf8358b6ab7810558242337980463c3d90
Scott Entwistle <scottyent@gmail.com>
2022-11-04 18:14:46 +0100

    Add small problems reader.rb solved

---
+++ RB120/small_problems/OO_1.rb/reader.rb
@@ -0,0 +1,18 @@
+# Using the code from the previous exercise, add a getter method named
+# #name and invoke it in place of @name in #greet.
+
+class Cat
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def greet
+    puts "Hello! My name is #{name}!"
+  end
+end
+
+kitty = Cat.new('Sophie')
+kitty.greet
+p kitty.name
