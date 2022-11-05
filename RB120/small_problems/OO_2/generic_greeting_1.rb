5a9dfec13b951b9c4732a246caff3761d8975c2d
Scott Entwistle <scottyent@gmail.com>
2022-11-06 00:28:12 +0100

    Add small problems generic_greeting_1.rb solved

--- RB120/small_problems/OO_2/generic_greeting_1.rb
+++ RB120/small_problems/OO_2/generic_greeting_1.rb
@@ -1,1 +1,9 @@
 # Modify the following code so that Hello! I'm a cat! is printed when Cat.generic_greeting is invoked.
+
+class Cat
+  def self.generic_greeting
+    puts "Hello! I'm a cat!"
+  end
+end
+
+Cat.generic_greeting
