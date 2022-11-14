ded65ea20244eecf7b55a68453f873137ba96ea9
Scott Entwistle <scottyent@gmail.com>
2022-11-14 17:28:44 +0100

    Add score_object file to attempt score as a class

---
+++ RB120/lesson_2/score_object_attempt.rb
@@ -0,0 +1,182 @@
+# I'm creating this file to attempt the exploratino of using a Score class
+# would be better than just using the score attribute in Player
+# I guess I could have used another branch in Git, but really I don't want
+# to merge this likely, and so this can just be my scrap paper.
+
+
+require 'pry'
+require 'pry-byebug'
+
+class Move
+  include Comparable
+  VALUES = %w(rock paper scissors)
+  attr_reader :value
+
+  def initialize(value)
+    @value = value
+  end
+
+  def to_s
+    value
+  end
+
+  def scissors?
+    @value == 'scissors'
+  end
+
+  def rock?
+    @value == 'rock'
+  end
+
+  def paper?
+    @value == 'paper'
+  end
+
+  def >(other_player)
+    (rock? && other_player.scissors?) ||
+      (paper? && other_player.rock?) ||
+      (scissors? && other_player.paper?)
+  end
+
+  def <(other_player)
+    (rock? && other_player.paper?) ||
+      (paper? && other_player.scissors?) ||
+      (scissors? && other_player.rock?)
+  end
+end
+
+class Player
+  attr_accessor :move, :name, :score
+
+  def initialize
+    @name = set_name
+    @score = 0
+  end
+end
+
+class Human < Player
+  def set_name
+    n = ''
+    loop do
+      puts "What's your name?"
+      n = gets.chomp
+      break unless n.empty?
+      puts "That's not a valid name."
+    end
+    self.name = n
+  end
+
+  def choose
+    choice = nil
+    loop do
+      puts "Please choose rock, paper, or scissors:"
+      choice = gets.chomp
+      break if Move::VALUES.include?(choice)
+      puts "Invalid choice."
+    end
+    self.move = Move.new(choice)
+  end
+end
+
+class Computer < Player
+  def set_name
+    self.name = %w(Ultron Dominator Hal Chappie R2D2).sample
+  end
+
+  def choose
+    self.move = Move.new(Move::VALUES.sample)
+  end
+end
+
+# We need an orchestration engine - whatttt? This is interesting :D
+# Basically a class to... run the game flow? Kind of like, the outer logic
+# in the procedural flow we've been doing
+
+class RPSgame
+  attr_accessor :human, :computer
+  MAX_WINS = 3
+
+  def initialize
+    @human = Human.new
+    @computer = Computer.new
+  end
+
+  def play
+    display_welcome_message
+
+    loop do
+      human.choose
+      computer.choose
+      display_moves
+      display_winner
+      display_scoreboard
+      break if final_winner?
+      break unless play_again?
+    end
+    display_goodbye_message
+  end
+
+  def play_again?
+    loop do
+      puts "Do you want to play again?(y/n)"
+      choice = gets.chomp.downcase
+      return true if %w(y yes).include?(choice)
+      return false if %w(n no).include?(choice)
+      puts "I'm sorry, that's not a valid choice."
+    end
+  end
+
+  def final_winner?
+    human.score == MAX_WINS || computer.score == MAX_WINS
+  end
+
+  def display_moves
+    divider
+    puts "#{human.name} chose #{human.move}"
+    puts "#{computer.name} chose #{computer.move}"
+  end
+
+  def display_scoreboard
+    divider
+    puts "#{human.name}: #{human.score}"
+    puts "#{computer.name}: #{computer.score}"
+  end
+
+  def display_winner
+    if human.move > computer.move
+      puts "#{human.name} wins!"
+      human.score += 1
+    elsif human.move < computer.move
+      puts "#{computer.name} wins!"
+      computer.score += 1
+    else
+      puts "It's a tie."
+    end
+  end
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors #{human.name}!"
+    divider
+  end
+
+  def tournament_winner
+    return computer if computer.score > human.score
+    return human if human.score > computer.score
+  end
+
+
+  def display_goodbye_message
+    divider
+    if tournament_winner
+      puts "#{tournament_winner.name} has won the tournament with #{tournament_winner.score} games."
+    end
+    puts "Thanks for playing! Goodbye."
+  end
+
+  def divider
+    divider = '-' * 30
+    puts divider
+  end
+end
+
+RPSgame.new.play
