3e126f64c888d1e130c9a88b192999a9a12749b5
Scott Entwistle <scottyent@gmail.com>
2022-11-21 21:55:06 +0100

    Add q_5.rb solved

---
+++ RB120/lesson_4/easy_2/q_5.rb
@@ -0,0 +1,11 @@
+# There are a number of variables listed below.
+# What are the different types and how do you know which is which?
+
+excited_dog = "excited dog"  # => local variable
+@excited_dog = "excited dog" # => Instance Variable
+@@excited_dog = "excited dog" # => Class variable
+
+# You can tell based on the @ symbols in front of the variable. One is for
+# instance variables, two is for class variables, and none is a local variable.
+
+# Correct
