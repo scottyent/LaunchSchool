07e5a46d87a9a5b02ec75a73e1aa9817a6d00248
Scott Entwistle <scottyent@gmail.com>
2022-11-21 22:08:47 +0100

    Add q_10.rb solved

---
+++ RB120/lesson_4/easy_2/q_10.rb
@@ -0,0 +1,15 @@
+=begin
+What are the benefits of using Object Oriented Programming in Ruby?
+Think of as many as you can.
+
+1. Allows encapsulation so you can control how things are accessed and used
+from outside what you design.
+2. It provides a level of abstraction to make organizing things a bit easier,
+by making classes that represent real things in real life.
+3. It allows you to write in a DRY way, making much of your code easily reusable
+and ultimately, modular. It does this by inheritance, but also modules.
+
+They also had:
+4. As the software becomes more complex this complexity can be more
+easily managed.
+=end
