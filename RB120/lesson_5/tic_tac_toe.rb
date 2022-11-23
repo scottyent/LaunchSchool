41a5641da5e6b027db172d0bfe30472b0effc1b0
Scott Entwistle <scottyent@gmail.com>
2022-11-23 23:35:58 +0100

    Build out play flow up through human_moves

--- RB120/lesson_5/tic_tac_toe.rb
+++ RB120/lesson_5/tic_tac_toe.rb
@@ -2,7 +2,7 @@
 My own short description of the game:
 starting with an empty 3x3 board, the first player chooses where to put an x.
 The second player then decides where to put an O, and they alternate turns
-until either a tie, or one player has 3 in a row of their symbol.
+until either a tie, or one player has 3 in a row of their marker.
 
 Their description (somehwat similar but maybe a bit more succinct):
 Tic Tac Toe is a 2-player board game played on a 3x3 grid. Players take turns
@@ -29,21 +29,42 @@ Player
 
 # Spike
 
+require 'pry'
+require 'pry-byebug'
+
 class Board
+  INITIAL_MARKER = " "
+  attr_reader :squares
   def initialize
-    # Perhaps initializing an enpty board 3x3
-    # A data structure to keep track of those difference areas of the board
-    # array/hash of string objects?
-    # array/hash of Square objects?
+    @squares = empty_board
   end
 
-  def display
+  def empty_board
+    arr = []
+    9.times { |_| arr << Square.new(INITIAL_MARKER) }
+    arr
+  end
+
+  def get_square_at(square_number)
+    index = square_number - 1
+    squares[index]
+  end
+
+  def set_square_at(square_number, marker)
+    index = square_number - 1
+    squares[index].marker = marker
   end
 end
 
 class Square
-  def initialize
-    # This one I' not sure about. Maybe square state, set to blank space?
+  attr_writer :marker
+
+  def initialize(marker)
+    @marker = marker
+  end
+
+  def to_s
+    @marker
   end
 
   def update(input_marker)
@@ -51,31 +72,82 @@ class Square
 end
 
 class Player
-  def initialize
-  # Maybe their choices
-  # the symbol assigned to them
-  end
+  attr_reader :marker
 
-  def mark
+  def initialize(marker)
+    @marker = marker
   end
 end
 
 # Orchestration Engine
 
 class TTTGame
-  def play
+  attr_reader :board, :human, :computer
+  def initialize
+    @board = Board.new
+    @human = Player.new("X")
+    @computer = Player.new("O")
+  end
+
+  def display_welcome_message
+    puts "Welcome to Tic Tac Toe!"
+    divider
+  end
+
+  def display_goodbye_message
+    divider
+    puts "Thanks for playing Tic Tac Toe! Goodbye."
+  end
+
+  def divider
+    puts "-" * 17
+  end
+
+  def display_board(board)
+    puts
+    puts "     |     |     "
+    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.
+      get_square_at(3)}"
+    puts "     |     |     "
+    puts "-----------------"
+    puts "     |     |     "
+    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.
+      get_square_at(6)}"
+    puts "     |     |     "
+    puts "-----------------"
+    puts "     |     |     "
+    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.
+      get_square_at(9)}"
+    puts "     |     |     "
+    puts
+  end
+
+  def human_moves
+    puts "Choose a square between 1-9"
+    square = nil
     loop do
+      square = gets.chomp.to_i
+      break if (1..9).include?(square)
+      puts "Sorry, that's not a valid choice."
+    end
+
+    # binding.pry
+    board.set_square_at(square, human.marker)
+  end
+
+  def play
+    # loop do
       display_welcome_message
       loop do
-        display_board
-        player1.mark
-        break if winner? || board_full
-        player2.mark
-        break if winner? || board_full
+        display_board(board)
+        human_moves
+        # break if winner? || board_full
+        # computer_moves
+        # break if winner? || board_full
       end
-      display_result
-      break unless play_again?
-    end
+      # display_result
+    #   break unless play_again?
+    # end
     display_goodbye_message
   end
 end
