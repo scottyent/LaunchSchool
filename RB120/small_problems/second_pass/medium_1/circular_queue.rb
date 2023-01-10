3cad2297cf5e14204c7bac14187bc1d3313e7a6e
Scott Entwistle <scottyent@gmail.com>
2023-01-11 00:08:42 +0100

    Add problem 4 solved

---
+++ RB120/small_problems/second_pass/medium_1/circular_queue.rb
@@ -0,0 +1,59 @@
+class CircularQueue
+
+  attr_reader :queue
+
+  def initialize(size)
+    @queue = Array.new
+    @max = size
+  end
+
+  def dequeue
+    return nil if queue.empty?
+    queue.shift
+  end
+
+  def enqueue(item)
+    queue.shift if queue.size == @max
+    @queue << item
+  end
+end
+
+# Testing
+queue = CircularQueue.new(3)
+puts queue.dequeue == nil
+
+queue.enqueue(1)
+queue.enqueue(2)
+puts queue.dequeue == 1
+
+queue.enqueue(3)
+queue.enqueue(4)
+puts queue.dequeue == 2
+
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(7)
+puts queue.dequeue == 5
+puts queue.dequeue == 6
+puts queue.dequeue == 7
+puts queue.dequeue == nil
+
+queue = CircularQueue.new(4)
+puts queue.dequeue == nil
+
+queue.enqueue(1)
+queue.enqueue(2)
+puts queue.dequeue == 1
+
+queue.enqueue(3)
+queue.enqueue(4)
+puts queue.dequeue == 2
+
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(7)
+puts queue.dequeue == 4
+puts queue.dequeue == 5
+puts queue.dequeue == 6
+puts queue.dequeue == 7
+puts queue.dequeue == nil
