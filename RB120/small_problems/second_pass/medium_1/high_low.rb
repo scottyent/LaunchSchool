b0632268eae3d9a361bec6fd785a7c5998861af0
Scott Entwistle <scottyent@gmail.com>
2023-01-12 23:39:02 +0100

    Add problem 8 finished

---
+++ RB120/small_problems/second_pass/medium_1/high_low.rb
@@ -0,0 +1,80 @@
+require 'pry'
+require 'pry-byebug'
+
+class Card
+  include Comparable
+
+  FACE_CARDS = %w(Jack Queen King Ace)
+  FACE_TO_NUM = {
+    'Jack' => 11,
+    'Queen' => 12,
+    'King' => 13,
+    'Ace' => 14
+  }
+  attr_reader :rank, :suit
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def to_s
+    "#{rank} of #{suit}"
+  end
+
+  def rank_to_num(value)
+    FACE_CARDS.include?(value) ? FACE_TO_NUM[value] : value
+  end
+
+
+  def <=>(other)
+    card1 = rank_to_num(rank)
+    card2 = rank_to_num(other.rank)
+    card1 <=> card2
+  end
+end
+
+# Tests
+cards = [Card.new(2, 'Hearts'),
+  Card.new(10, 'Diamonds'),
+  Card.new('Ace', 'Clubs')]
+puts cards
+puts cards.min == Card.new(2, 'Hearts')
+puts cards.max == Card.new('Ace', 'Clubs')
+
+cards = [Card.new(5, 'Hearts')]
+puts cards.min == Card.new(5, 'Hearts')
+puts cards.max == Card.new(5, 'Hearts')
+
+cards = [Card.new(4, 'Hearts'),
+  Card.new(4, 'Diamonds'),
+  Card.new(10, 'Clubs')]
+puts cards.min.rank == 4
+puts cards.max == Card.new(10, 'Clubs')
+
+cards = [Card.new(7, 'Diamonds'),
+  Card.new('Jack', 'Diamonds'),
+  Card.new('Jack', 'Spades')]
+puts cards.min == Card.new(7, 'Diamonds')
+puts cards.max.rank == 'Jack'
+
+cards = [Card.new(8, 'Diamonds'),
+  Card.new(8, 'Clubs'),
+  Card.new(8, 'Spades')]
+puts cards.min.rank == 8
+puts cards.max.rank == 8
+
+# Output
+# 2 of Hearts
+# 10 of Diamonds
+# Ace of Clubs
+# true
+# true
+# true
+# true
+# true
+# true
+# true
+# true
+# true
+# true
