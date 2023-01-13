e2d7b074e7b5dcbf76b51921e2c841e0cd7dc021
Scott Entwistle <scottyent@gmail.com>
2023-01-13 16:41:29 +0100

    Add problem 9 completed

--- RB120/small_problems/second_pass/medium_1/deck_of_cards.rb
+++ RB120/small_problems/second_pass/medium_1/deck_of_cards.rb
@@ -2,10 +2,38 @@
 # contains all of the standard 52 playing cards. Use the following code to
 # start your work:
 
+require 'pry'
+require 'pry-byebug'
+
 
 class Deck
   RANKS = ((2..10).to_a + %w(Jack Queen King Ace)).freeze
   SUITS = %w(Hearts Clubs Diamonds Spades).freeze
+
+  def initialize
+    @deck_holder = new_deck
+  end
+
+  def new_deck
+    temp_deck = []
+
+    SUITS.each do |suit|
+      RANKS.each do |rank|
+        temp_deck << Card.new(rank, suit)
+      end
+    end
+
+    temp_deck.shuffle
+  end
+
+  def draw
+    self.deck_holder = new_deck if deck_holder.empty?
+    deck_holder.pop
+  end
+
+  private
+
+  attr_accessor :deck_holder
 end
 
 class Card
@@ -48,9 +76,9 @@ end
 deck = Deck.new
 drawn = []
 52.times { drawn << deck.draw }
-drawn.count { |card| card.rank == 5 } == 4
-drawn.count { |card| card.suit == 'Hearts' } == 13
+p drawn.count { |card| card.rank == 5 } == 4
+p drawn.count { |card| card.suit == 'Hearts' } == 13
 
 drawn2 = []
 52.times { drawn2 << deck.draw }
-drawn != drawn2 # Almost always.
+p drawn != drawn2 # Almost always.
