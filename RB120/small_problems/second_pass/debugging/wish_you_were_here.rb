cc3d6561dd407134f0f407eef144be4cbf501c9d
Scott Entwistle <scottyent@gmail.com>
2023-01-13 23:26:41 +0100

    Add Debugging problem 3 solved

---
+++ RB120/small_problems/second_pass/debugging/wish_you_were_here.rb
@@ -0,0 +1,51 @@
+# On lines 37 and 38 of our code, we can see that grace and ada are located at
+# the same coordinates. So why does line 39 output false? Fix the code to
+# produce the expected output.
+
+class Person
+  attr_reader :name
+  attr_accessor :location
+
+  def initialize(name)
+    @name = name
+  end
+
+  def teleport_to(latitude, longitude)
+    @location = GeoLocation.new(latitude, longitude)
+  end
+end
+
+class GeoLocation
+  include Comparable
+
+  attr_reader :latitude, :longitude
+
+  def initialize(latitude, longitude)
+    @latitude = latitude
+    @longitude = longitude
+  end
+
+  def to_s
+    "(#{latitude}, #{longitude})"
+  end
+
+  def <=>(other_loc)
+    self.to_s <=> other_loc.to_s
+  end
+
+end
+
+# Example
+
+ada = Person.new('Ada')
+ada.location = GeoLocation.new(53.477, -2.236)
+
+grace = Person.new('Grace')
+grace.location = GeoLocation.new(-33.89, 151.277)
+
+ada.teleport_to(-33.89, 151.277)
+
+puts ada.location                   # (-33.89, 151.277)
+puts grace.location                 # (-33.89, 151.277)
+puts ada.location == grace.location # expected: true
+                                    # actual: false
