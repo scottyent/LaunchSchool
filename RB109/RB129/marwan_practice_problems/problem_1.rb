6f9b137109bd083ff28e50598cf8a57ebb77b33d
Scott Entwistle <scottyent@gmail.com>
2023-01-15 15:17:11 +0100

    Add problem 1 completed

--- RB109/RB129/marwan_practice_problems/problem_1.rb
+++ RB109/RB129/marwan_practice_problems/problem_1.rb
@@ -1,1 +1,21 @@
 # What will the following code output? Why?
+
+class Student
+  attr_reader :id
+
+  def initialize(name)
+    @name = name
+    @id
+  end
+
+  def id=(value)
+    self.id = value
+  end
+end
+
+tom = Student.new("Tom")
+tom.id = 45
+
+# The reason this causes an error is it's basically calling itself. self.id
+# is calling the setter method for @id, but it's calling it within the actual
+# setter method itself, so it will recursively call forever causing a SystemStackError
