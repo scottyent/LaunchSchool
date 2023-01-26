b880e43a4e6d50f38e24d68b6e7f93f22fd19fb6
Scott Entwistle <scottyent@gmail.com>
2023-01-26 22:28:17 +0100

    Add interview practice problem 1 solved

---
+++ RB129/pratice_interview_problems/problem_1.rb
@@ -0,0 +1,26 @@
+module Presentable
+  def to_s
+    "My name is #{@name} and I am a #{@job}"
+  end
+end
+
+class Person
+  include Presentable
+
+  def initialize(name, job)
+      @name = name
+      @job = job
+  end
+end
+
+roger = Person.new("Roger", "Carpenter")
+puts roger
+
+# Add 1 line of code for the person class
+# and 1 line of code in the initalize method.
+
+
+# Other than that don't change Person.
+
+# Output:
+# "My name is Roger and I am a Carpenter"
