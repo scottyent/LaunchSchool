2967f545534da0a5ed7d12d8a25f85a215a2b787
Scott Entwistle <scottyent@gmail.com>
2023-02-13 22:34:40 +0100

    Add block_scope exercise completed

---
+++ RB130/lesson_1/block_scope.rb
@@ -0,0 +1,9 @@
+def call_me(some_code)
+  some_code.call
+end
+
+name = "Robert"
+chunk_of_code = Proc.new {puts "hi #{name}"}
+name = "Griffin III"        # re-assign name after Proc initialization
+
+call_me(chunk_of_code)
