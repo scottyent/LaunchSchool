360d2bc78efa0cab5274a73ec9b0e1a1ba1e117f
Scott Entwistle <scottyent@gmail.com>
2023-02-14 22:24:11 +0100

    Add additional assertions to test file

--- RB130/lesson_2/car_test.rb
+++ RB130/lesson_2/car_test.rb
@@ -10,8 +10,33 @@ class CarTest < MiniTest::Test
     assert_equal(4, car.wheels)
   end
 
-  def test_bad_wheels
+  def test_car_exists
     car = Car.new
-    assert_equal(4, car.wheels)
+    assert(car)
+  end
+
+  def test_name_is_nil
+    car = Car.new
+    assert_nil(car.name)
+  end
+
+  def test_raise_initialize_with_arg
+    assert_raises(ArgumentError) do
+      car = Car.new(name: "Joey")         # this code raises ArgumentError, so this assertion passes
+    end
+  end
+
+  def test_instance_of_car
+    car = Car.new
+    assert_instance_of(Car, car)
+  end
+
+  def test_includes_car
+    car = Car.new
+    arr = [1, 2, 3]
+    arr << car
+
+    assert_includes(arr, car)
   end
 end
+
