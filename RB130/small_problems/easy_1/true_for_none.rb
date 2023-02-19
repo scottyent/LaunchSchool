d5a7d527fd1898cadf4ed6aa9f25b18389611f03
Scott Entwistle <scottyent@gmail.com>
2023-02-19 22:45:05 +0100

    Add easy_1 problem 8 solved

---
+++ RB130/small_problems/easy_1/true_for_none.rb
@@ -0,0 +1,20 @@
+# Implementing a Enumerable#none? method
+
+
+def none?(collection)
+  return true if collection.empty?
+
+  collection.each { |item| return false if yield(item) }
+  true
+end
+
+
+
+# Test Cases
+p none?([1, 3, 5, 6]) { |value| value.even? } == false
+p none?([1, 3, 5, 7]) { |value| value.even? } == true
+p none?([2, 4, 6, 8]) { |value| value.odd? } == true
+p none?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false
+p none?([1, 3, 5, 7]) { |value| true } == false
+p none?([1, 3, 5, 7]) { |value| false } == true
+p none?([]) { |value| true } == true
