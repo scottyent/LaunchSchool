a2ef99c444ebf2eece7bc7e6252a89853b87de30
Scott Entwistle <scottyent@gmail.com>
2023-02-19 22:38:11 +0100

    Add easy_1 problem 7 solved

---
+++ RB130/small_problems/easy_1/true_for_all.rb
@@ -0,0 +1,17 @@
+# Implementing a #all? method, which is effectively the opposite of any?
+
+def all?(collection)
+  collection.each { |item| return false unless yield(item) }
+  true
+end
+
+
+# Test Cases
+
+p all?([1, 3, 5, 6]) { |value| value.odd? } == false
+p all?([1, 3, 5, 7]) { |value| value.odd? } == true
+p all?([2, 4, 6, 8]) { |value| value.even? } == true
+p all?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false
+p all?([1, 3, 5, 7]) { |value| true } == true
+p all?([1, 3, 5, 7]) { |value| false } == false
+p all?([]) { |value| false } == true
