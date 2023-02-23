c851a4c6f8627810ca6606e610dc9e6e89686a6d
Scott Entwistle <scottyent@gmail.com>
2023-02-23 09:12:54 +0100

    Add easy_2 problem 7 solved

---
+++ RB130/small_problems/easy_2/each_object.rb
@@ -0,0 +1,33 @@
+# recreate #each_with_object
+# takes an array, passes the elements to the block along with access to
+# a new specified object
+
+def each_with_object(arr, collection)
+  arr.each do |item|
+    yield(item, collection)
+  end
+
+  collection
+end
+
+
+# Test Cases
+result = each_with_object([1, 3, 5], []) do |value, list|
+  list << value**2
+end
+p result == [1, 9, 25]
+
+result = each_with_object([1, 3, 5], []) do |value, list|
+  list << (1..value).to_a
+end
+p result == [[1], [1, 2, 3], [1, 2, 3, 4, 5]]
+
+result = each_with_object([1, 3, 5], {}) do |value, hash|
+  hash[value] = value**2
+end
+p result == { 1 => 1, 3 => 9, 5 => 25 }
+
+result = each_with_object([], {}) do |value, hash|
+  hash[value] = value * 2
+end
+p result == {}
