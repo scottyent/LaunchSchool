152093ad5855add5f8f432e939ef53c984946f1c
Scott Entwistle <scottyent@gmail.com>
2023-02-23 09:52:52 +0100

    Add easy_2 problem 10 solved

---
+++ RB130/small_problems/easy_2/each_con_2.rb
@@ -0,0 +1,44 @@
+# Update our each_cons method to take any size of array slices, versus just
+# specifically 2
+
+def each_cons(arr, length)
+  index = 0
+
+  while index <= arr.size - length
+    yield(*arr[index, length])
+    index += 1
+  end
+
+  nil
+end
+
+# Test Cases
+hash = {}
+each_cons([1, 3, 6, 10], 1) do |value|
+  hash[value] = true
+end
+p hash == { 1 => true, 3 => true, 6 => true, 10 => true }
+
+hash = {}
+each_cons([1, 3, 6, 10], 2) do |value1, value2|
+  hash[value1] = value2
+end
+p hash == { 1 => 3, 3 => 6, 6 => 10 }
+
+hash = {}
+each_cons([1, 3, 6, 10], 3) do |value1, *values|
+  hash[value1] = values
+end
+p hash == { 1 => [3, 6], 3 => [6, 10] }
+
+hash = {}
+each_cons([1, 3, 6, 10], 4) do |value1, *values|
+  hash[value1] = values
+end
+p hash == { 1 => [3, 6, 10] }
+
+hash = {}
+each_cons([1, 3, 6, 10], 5) do |value1, *values|
+  hash[value1] = values
+end
+p hash == {}
