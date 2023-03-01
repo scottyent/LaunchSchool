e22ea06264e62f4b507c588fbaaa517105b78f1a
Scott Entwistle <scottyent@gmail.com>
2023-03-01 22:50:05 +0100

    Add accept_money method test

--- RB130/small_problems/medium_testing/initial_setup.rb
+++ RB130/small_problems/medium_testing/initial_setup.rb
@@ -3,6 +3,21 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 require_relative 'cash_register'
+require_relative 'transaction'
 
-class RegisterTest < Minitest::Test
+class CashRegisterTest < Minitest::Test
+  def setup
+    @kasse1 = CashRegister.new(500)
+    @transaction1 = Transaction.new(20)
+  end
+
+  def test_accept_money
+    @transaction1.amount_paid = 20
+
+    before_transaction = @kasse1.total_money
+    @kasse1.accept_money(@transaction1)
+    after_transaction = @kasse1.total_money
+
+    assert_equal before_transaction + 20, after_transaction
+  end
 end
