d8b3157c05934be478d2807ef801eade73d8341b
Scott Entwistle <scottyent@gmail.com>
2023-03-02 23:18:42 +0100

    Add test for prompt_for_payment

--- RB130/small_problems/medium_testing/transaction_tests.rb
+++ RB130/small_problems/medium_testing/transaction_tests.rb
@@ -2,8 +2,16 @@ require 'minitest/autorun'
 require "minitest/reporters"
 Minitest::Reporters.use!
 
-require 'transaction'
-require 'cash_register'
+require_relative 'transaction'
+require_relative 'cash_register'
+require 'stringio'
 
 class TransactionTest < Minitest::Test
+  def test_prompt_for_payment
+    purchase = Transaction.new(30)
+    input = StringIO.new("30\n")
+    purchase.prompt_for_payment(input: input)
+
+    assert_equal purchase.amount_paid, 30
+  end
 end
