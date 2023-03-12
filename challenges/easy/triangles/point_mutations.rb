29776cd6eea8a85ba825d60f06d07b8eec63c70b
Scott Entwistle <scottyent@gmail.com>
2023-03-12 23:13:03 +0100

    Setup code for second challenge

---
+++ challenges/easy/triangles/point_mutations.rb
@@ -0,0 +1,21 @@
+Write a program that can calculate the Hamming distance between two DNA strands.
+
+=begin
+Input:
+Output:
+
+Rules
+Explicit:
+*
+Implicit:
+*
+
+Algorithm:
+*
+=end
+
+def
+
+end
+
+# Test Cases
