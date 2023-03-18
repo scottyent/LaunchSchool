c9a14b355bc56116fc16b15c3b656040b8e0d9ba
Scott Entwistle <scottyent@gmail.com>
2023-03-18 23:08:49 +0100

    Add roman_numeral challenge solved and refactored for simplicity

--- challenges/easy/triangles/roman_numerals.rb
+++ challenges/easy/triangles/roman_numerals.rb
@@ -44,24 +44,68 @@ Algorithm:
 
 
   when iterating, you can just convert as it goes so 9 in the hundreds, convert
-    to 900 in roman numerals, then 8 in the tens, convert that, then convert
+    to 900 in roman numerals, then 8 in the tens, convert that to 80, then convert
     single digits.
 
-  so you add to the roman_numeral variable each iteration (and skip 0)
+  so you add to the roman variable each iteration (and skip 0)
+
+  ok so the algoritm would be
+
+  if the number 1-3, then multiply the place by the roman numeral representation.
+  if the number is 4, original number subtracted from mid number
+  if it's 5 just convert to mid number
+  if it is 6-8 do mid number number modulo 5 times the original roman repre
+  if 9 use the next up place and subtract from there.
+
+  I believe I could use a dictionary describing the placees (ones, tens etc)
+  and then access the base + the mid point as needed. How do I know when base
+  to use...
+
+  how about an array that shows the bases (which are keys) equal to the size
+  of the number? ok - then I could access that with size - 1. So starting
+  index for a 4 place number, is 3 (thousands), then once you iterate past that
+  subtract a number to get 2 (hundreds), and so on.
+  ["ones", "tens", "hundreds", "thousands"]
+
 =end
 
 class RomanNumeral
+  PLACES = ["ones", "tens", "hundreds", "thousands"]
+  CONVERTER = {
+    "ones" => {base: 'I', middle: 'V'},
+    "tens" =>   {base: 'X', middle: 'L'},
+    "hundreds" => {base: 'C', middle: 'D'},
+    "thousands" => {base: 'M'}
+  }
+
   def initialize(decimal)
-    @decimal = decimal
+    @decimal_array = decimal.to_s.chars.map(&:to_i)
   end
 
   def to_roman
-    roman_numeral = ''
+    roman = ''
+    place_index = @decimal_array.size - 1
 
-    roman_numeral # or use each with object and let that return the string
+    @decimal_array.each do |digit|
+      conversion = convert(digit, place_index)
+      roman << conversion if conversion
+      place_index -= 1
+    end
+
+    roman
+  end
+
+  private
+
+  def convert(value, index)
+    place = PLACES[index]
+
+    case value
+    when 1..3 then CONVERTER[place][:base] * value
+    when 4 then CONVERTER[place][:base] + CONVERTER[place][:middle]
+    when 5 then CONVERTER[place][:middle]
+    when 6..8 then CONVERTER[place][:middle] + (CONVERTER[place][:base] * (value % 5))
+    when 9 then CONVERTER[place][:base] + CONVERTER[PLACES[index + 1]][:base]
+    end
   end
 end
-
-end
-
-# Test Cases
