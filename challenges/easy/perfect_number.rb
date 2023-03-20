71ecb467488f512856fffbda76ae76173c8c6988
Scott Entwistle <scottyent@gmail.com>
2023-03-20 23:28:14 +0100

    Add setup for perfect number challenge including tests

---
+++ challenges/easy/perfect_number.rb
@@ -0,0 +1,16 @@
+=begin
+The Greek mathematician Nicomachus devised a classification scheme for natural
+numbers (1, 2, 3, ...), identifying each as belonging uniquely to the categories
+of abundant, perfect, or deficient based on a comparison between the number and
+the sum of its positive divisors (the numbers that can be evenly divided into
+  the target number with no remainder, excluding the number itself).
+  For instance, the positive divisors of 15 are 1, 3, and 5. This sum is known
+  as the Aliquot sum.
+
+Perfect numbers have an Aliquot sum that is equal to the original number.
+Abundant numbers have an Aliquot sum that is greater than the original number.
+Deficient numbers have an Aliquot sum that is less than the original number.
+
+Write a program that can tell whether a number is perfect, abundant,
+or deficient.
+=end
