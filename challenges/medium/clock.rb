a09bc20aa55af492ccb019387f4b2200ad6a0406
Scott Entwistle <scottyent@gmail.com>
2023-04-19 23:27:53 +0200

    Add setup for clock challenge

---
+++ challenges/medium/clock.rb
@@ -0,0 +1,10 @@
+# Create a clock that is independent of date.
+
+# You should be able to add minutes to and subtract minutes from the time
+# represented by a given Clock object. Note that you should not mutate
+# Clock objects when adding and subtracting minutes -- create a new Clock object.
+
+# Two clock objects that represent the same time should be equal to each other.
+
+# You may not use any built-in date or time functionality; just use arithmetic
+# operations.
