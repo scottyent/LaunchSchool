7b8fc6567ffb51312842a1350183db3c50252955
Scott Entwistle <scottyent@gmail.com>
2024-09-25 10:57:03 +0200

    Add my working solution for asking user to continue

--- PY101/lesson_2/calculator/calculator.py
+++ PY101/lesson_2/calculator/calculator.py
@@ -1,7 +1,3 @@
-# Ask the user for the operation they want to perform (+,-,*,/).
-# Perform the operation on the two numbers.
-# Display the result to the user.
-
 def prompt(text):
     print(f'==> {text}')
 
@@ -16,48 +12,65 @@ def valid_number(str_int):
 def invalid_operation(operation_str):
     return not operation_str in ['1', '2', '3', '4']
 
+def calculate_again():
+    prompt('Do you want to perform another calculation? y/n')
+    answer = input().casefold()
+
+    while answer not in ['y', 'n']:
+        prompt('That is not a valid response.')
+        answer = input()
+
+    return True if answer == 'y' else False
+
 print('Welcome to the calculator!')
 
-# Ask the user for the first number.
-num1_not_set = True # pylint: disable-msg=C0103
-while num1_not_set:
-    prompt('What is the first number?')
-    num1 = input()
-    num1, is_valid = valid_number(num1)
-    if is_valid:
-        num1_not_set = False # pylint: disable-msg=C0103
+continue_calculating = True
 
-# Ask the user for the second number.
-num2_not_set = True # pylint: disable-msg=C0103
-while num2_not_set:
-    prompt('What is the second number?')
-    num2 = input()
-    num2, is_valid = valid_number(num2)
-    if is_valid:
-        num2_not_set = False # pylint: disable-msg=C0103
+while continue_calculating:
+    # Ask the user for the first number.
+    num1_not_set = True # pylint: disable-msg=C0103
+    while num1_not_set:
+        prompt('What is the first number?')
+        num1 = input()
+        num1, is_valid = valid_number(num1)
+        if is_valid:
+            num1_not_set = False # pylint: disable-msg=C0103
 
-prompt(f'Great! We\'ll be performing an operation with {num1} and {num2}')
+    # Ask the user for the second number.
+    num2_not_set = True # pylint: disable-msg=C0103
+    while num2_not_set:
+        prompt('What is the second number?')
+        num2 = input()
+        num2, is_valid = valid_number(num2)
+        if is_valid:
+            num2_not_set = False # pylint: disable-msg=C0103
 
-# Ask what operation
-prompt("""What operation would you like to perform?
-    1) Add 2) Subtract 3) Multiply 4) Divide"""
-    )
-operation = input()
+    prompt(f'Great! We\'ll be performing an operation with {num1} and {num2}')
 
-while invalid_operation(operation):
-    prompt('Whoopsies! That is not a valid operation. Try again.')
+    # Ask what operation
+    prompt("""What operation would you like to perform?
+        1) Add 2) Subtract 3) Multiply 4) Divide"""
+        )
     operation = input()
 
-match operation:
-    case '1':
-        result = num1 + num2
-    case '2':
-        result = num1 - num2
-    case '3':
-        result = num1 * num2
-    case '4':
-        result = num1 / num2
-    case _:
-        prompt("You didn't choose an appropriate operation!")
+    while invalid_operation(operation):
+        prompt('Whoopsies! That is not a valid operation. Try again.')
+        operation = input()
 
-prompt(f'The result is: {result}')
+    match operation:
+        case '1':
+            result = num1 + num2
+        case '2':
+            result = num1 - num2
+        case '3':
+            result = num1 * num2
+        case '4':
+            result = num1 / num2
+        case _:
+            prompt("You didn't choose an appropriate operation!")
+
+    prompt(f'The result is: {result}')
+
+    if not calculate_again():
+        prompt("Thanks for using the calculator. Goodbye!")
+        continue_calculating = False
