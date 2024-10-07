3d7eb6f78d201b4c3a55d331406e203bc6eae3eb
Scott Entwistle <scottyent@gmail.com>
2024-10-07 11:05:48 +0200

    Add best of 5 feature

--- PY101/lesson_2/rock_paper_scissors/rock_paper_scissors.py
+++ PY101/lesson_2/rock_paper_scissors/rock_paper_scissors.py
@@ -1,6 +1,8 @@
 import random
 
 VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard']
+TOTAL_GAMES = 5
+GAMES_NEEDED_TO_WIN = (TOTAL_GAMES//2 + 1)
 
 BEATS = {
     'rock'    : ['scissors', 'lizard'],
@@ -11,13 +13,29 @@ BEATS = {
 }
 
 def prompt(message):
-    print(f'==>{message}')
+    print(f'==> {message}')
+
+def display_separator():
+    print('---------------')
 
 def user_wins(player, computer):
     return computer in BEATS[player]
 
 def computer_wins(player, computer):
     return player in BEATS[computer]
+
+def update_scoreboard(player, computer, current_scoreboard):
+    if user_wins(player, computer):
+        current_scoreboard['Player'] += 1
+    elif computer_wins(player, computer):
+        current_scoreboard['Computer'] += 1
+
+def display_scoreboard(current_scoreboard):
+    prompt('Scoreboard')
+    display_separator()
+    for player, score in current_scoreboard.items():
+        prompt(f'{player}: {score}')
+    display_separator()
 
 def display_winner(player, computer):
     if user_wins(player, computer):
@@ -26,6 +44,16 @@ def display_winner(player, computer):
         prompt('Computer wins.')
     else:
         prompt('It\'s a tie!')
+
+def display_champion(current_scoreboard):
+    display_separator()
+    prompt('Game over!')
+    display_scoreboard(current_scoreboard)
+
+    for player, score in current_scoreboard.items():
+        if score == GAMES_NEEDED_TO_WIN:
+            prompt(f'{player} won {GAMES_NEEDED_TO_WIN} games '
+                   'and is the champion! \U0001F389')
 
 def ask_spock_or_scissors():
     prompt('You only entered one s. Did you mean Spock or Scissors?')
@@ -44,16 +72,29 @@ def match_choice(player_choice):
         return player_choice
 
     # Handle single character s choice
-    if player_choice.startswith('s') and len(player_choice) == 1:
+    if player_choice == 's':
         player_choice = ask_spock_or_scissors()
 
     for move in VALID_CHOICES:
         if move.startswith(player_choice):
             return move
 
-    return "Failed to match"
+    return None
 
-prompt(f'Welcome to the {', '.join(VALID_CHOICES)} game!')
+def welcome_to_game():
+    display_separator()
+    prompt(f'Welcome to the {', '.join(VALID_CHOICES)} game!')
+    prompt(f'If you choose to continue playing, the first to {GAMES_NEEDED_TO_WIN}'
+        ' games wins the championship.')
+    prompt('Good luck!')
+    display_separator()
+
+welcome_to_game()
+
+scoreboard = {
+    'Player'  : 0,
+    'Computer': 0,
+}
 
 while True:
     prompt(f'Choose one: {', '.join(VALID_CHOICES)}')
@@ -67,6 +108,12 @@ while True:
     prompt(f'Your pick: {choice} |  Computer pick: {computer_choice}')
 
     display_winner(choice, computer_choice)
+    update_scoreboard(choice, computer_choice, scoreboard)
+
+    if GAMES_NEEDED_TO_WIN in scoreboard.values():
+        break
+
+    display_scoreboard(scoreboard)
 
     prompt('Do you want to play again? y/n')
     play_again = input().casefold()
@@ -79,3 +126,5 @@ while True:
 
     if play_again[0] == 'n':
         break
+
+display_champion(scoreboard)
